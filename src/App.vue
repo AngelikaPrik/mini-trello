91b8d1e35c789732bbc26ef7e59cbd4767da243a
Ангелина <pretzel318@gmail.com>
2025-09-10 19:59:04 +0200

    Отображение контента только при авторизации

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
-  <router-view />
-   <AuthButton />
+  <div>
+    <button v-if="!user" @click="loginGithub">Войти через GitHub</button>
+
+    <div v-if="user">
+      <BoardPage />
+      <button class="btn-logout" @click="logout">Выйти</button>
+    </div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
-import AuthButton from './components/ui/AuthButton.vue';
+import BoardPage from './pages/BoardPage.vue';
+import { supabase } from '@/lib/supabase'
+import { ref, onMounted } from 'vue'
+
+const user = ref<any>(null)
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getUser()
+  user.value = data.user
+
+  supabase.auth.onAuthStateChange((_event, session) => {
+    user.value = session?.user ?? null
+  })
+})
+
+async function loginGithub() {
+  await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: { redirectTo: window.location.origin },
+  })
+}
+
+async function logout() {
+  await supabase.auth.signOut()
+}
 
 </script>
 
-
-<style scoped></style>
+<style scoped>
+.btn-logout {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
