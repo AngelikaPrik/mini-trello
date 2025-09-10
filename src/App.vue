bfa07a05df55df4763d6bd124f41ef1b4fe380de
Ангелина <pretzel318@gmail.com>
2025-09-10 19:43:58 +0200

    Добавление авторизации через github

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
   <router-view />
+   <AuthButton />
 </template>
+
+<script setup lang="ts">
+import AuthButton from './components/ui/AuthButton.vue';
+
+</script>
+
 
 <style scoped></style>
